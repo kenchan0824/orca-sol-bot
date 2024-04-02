7a51a0df82d151ebe0d11ac60c5445ec70dc9313
Kendrick Chan <kenchan0824@gmail.com>
2024-04-02 13:07:26 +0800

    function to get postion details

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Subscribe to @OrcaSolBot on Telegram and input your Solana wallet address.
 
 - [x] ~~Function to list all LP postions owned by a wallet.~~
 - [x] ~~Function to query the symbol of a token minit address.~~
-- [ ] Function to retrieve the details of an LP position.
+- [x] ~~Function to retrieve the details of an LP position.~~
 - [ ] Set up a Telgram bot.
 - [ ] Bot listener to handle wallet address input and output list of LPs.
 - [ ] Force to use SOL or USDC as quote token.
--- utils/orca.js
+++ utils/orca.js
@@ -1,15 +1,17 @@
 import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
 import {
     WhirlpoolContext, ORCA_WHIRLPOOL_PROGRAM_ID, PDAUtil, IGNORE_CACHE
+    , buildWhirlpoolClient, PriceMath
 } from "@orca-so/whirlpools-sdk";
 import { TOKEN_PROGRAM_ID, unpackAccount } from "@solana/spl-token";
 import BN from "bn.js";
 import axios from 'axios';
 
 
-export function getContext() {
+export async function getContext() {
     const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
     const ctx = WhirlpoolContext.from(connection, {}, ORCA_WHIRLPOOL_PROGRAM_ID);
+    ctx.tokenSymbols = await listTokenSymbols();
     return ctx;
 }
 
@@ -19,14 +21,21 @@ export async function listPositionsByOwner(ctx, wallet_address) {
     );
 
     const pda_pubkeys = [];
+
+    // Get all token accounts
     tokenAccounts.forEach(({ account, pubkey}) => {
         const parsed = unpackAccount(pubkey, account);
+        
+        // Ignores empty token accounts and non-NFTs
         if (new BN(parsed.amount.toString()).eq(new BN(1))) {
+            
+            // Derive the address of Whirlpool's position from the mint address
             const pda = PDAUtil.getPosition(ctx.program.programId, parsed.mint);
             pda_pubkeys.push(pda.publicKey);
         }
     });
 
+    /// Try to get data from Whirlpool position addresses
     const candidates = await ctx.fetcher.getPositions(pda_pubkeys, IGNORE_CACHE);
     const position_addresses = [];
     pda_pubkeys.forEach((pubkey) => {
@@ -46,4 +55,29 @@ export async function listTokenSymbols() {
         if (symbol) tokensSymbols[mint] = symbol;
     })
     return tokensSymbols;
+}
+
+export async function getPositionDetails(ctx, position_address) {
+    const client = buildWhirlpoolClient(ctx);
+    const pubkey = new PublicKey(position_address);
+    const position = (await client.getPosition(pubkey)).getData();
+    
+    // Get the pool to which the position belongs
+    const pool = await client.getPool(position.whirlpool);
+    const token_a = pool.getTokenAInfo();
+    const token_b = pool.getTokenBInfo();
+
+    const pool_price = PriceMath.sqrtPriceX64ToPrice(pool.getData().sqrtPrice, token_a.decimals, token_b.decimals);
+    
+    // Get the price range of the position
+    const lower_price = PriceMath.tickIndexToPrice(position.tickLowerIndex, token_a.decimals, token_b.decimals);
+    const upper_price = PriceMath.tickIndexToPrice(position.tickUpperIndex, token_a.decimals, token_b.decimals);
+
+    return {
+        token_a: ctx.tokenSymbols[token_a.mint.toBase58()]
+        , token_b: ctx.tokenSymbols[token_b.mint.toBase58()]
+        , pool_price
+        , lower_price
+        , upper_price
+    }
 }
