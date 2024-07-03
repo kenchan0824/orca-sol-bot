9ba5aec635765234c7a5debc8f067c882d3739d2
Kendrick Chan <kenchan0824@gmail.com>
2024-07-03 08:06:01 +0800

    response immediately

--- handler.js
+++ handler.js
@@ -1,17 +1,17 @@
-import { getContext, listPositionsByOwner, getPositionsDetails} from './utils/orca.js';
+import { getContext, listPositionsByOwner, getPositionsDetails } from './utils/orca.js';
 import { format } from './utils/range.js';
 
 
 export function start_handler(ctx) {
 
     const msg = "*Orca Alert Bot*\n" +
-    "We notify when your Orca 🐳 LPs are out of range\\.\n" +
-    "No false alarm\\. 🚨 No NFT 🗿 required\\.\n\n" +
-    "*How To Use*\n" +
-    "Paste your Solana wallet 💰 address to start with\\. We'll list all your Orca 🐳 LP positions\\.\n\n" +
-    "The number beside ⚽️ indicates the buffer to the nearest boundary 🗑, when the position is in range ✅\\. " +
-    "Otherwise 🚫, it shows how the current price ⚽️ derivates from the boundary 🗑\\.\n\n" +
-    "Keep calm and stay farming\\. We'll notify 🔔 you when the time comes, but only for the last address inputted\\."
+        "We notify when your Orca 🐳 LPs are out of range\\.\n" +
+        "No false alarm\\. 🚨 No NFT 🗿 required\\.\n\n" +
+        "*How To Use*\n" +
+        "Paste your Solana wallet 💰 address to start with\\. We'll list all your Orca 🐳 LP positions\\.\n\n" +
+        "The number beside ⚽️ indicates the buffer to the nearest boundary 🗑, when the position is in range ✅\\. " +
+        "Otherwise 🚫, it shows how the current price ⚽️ derivates from the boundary 🗑\\.\n\n" +
+        "Keep calm and stay farming\\. We'll notify 🔔 you when the time comes, but only for the last address inputted\\."
 
     ctx.reply(msg, { parse_mode: "MarkdownV2" });
 }
@@ -26,26 +26,26 @@ export async function address_handler(ctx, session) {
 
     try {
         const orca = await getContext();
+        await ctx.reply("🤔  Let me check your LPs ...");
         const position_keys = await listPositionsByOwner(orca, wallet_address);
 
-        if (!position_keys.length) {
+        if (position_keys.length) {    
+            const lps = await getPositionsDetails(orca, position_keys)
+            const lines = lps.map(lp => {
+                const out_range = lp.pool_price > lp.upper_price || lp.pool_price < lp.lower_price;
+                const range_text = format(lp.pool_price, lp.lower_price, lp.upper_price);
+                if (!out_range) candidates.push(lp.pda);
+                return `${out_range ? '🚫' : '✅'}  *${lp.token_a} \\- ${lp.token_b}*  ${range_text}`
+            })
+            await ctx.reply(lines.join('\n\n'), { parse_mode: "MarkdownV2" });
+    
+            if (candidates.length) {
+                await ctx.reply("🔔  Notification On");
+            }
+        } else {
             await ctx.reply("🤷🏻‍♂️  It seems you don't have any LPs.");
             await ctx.reply("🔕  Notification Off");
             return;
-        }
-        await ctx.reply("🤔  Let me check your LPs ...");
-
-        const lps = await getPositionsDetails(orca, position_keys)
-        const lines = lps.map(lp => {
-            const out_range = lp.pool_price > lp.upper_price || lp.pool_price < lp.lower_price;
-            const range_text = format(lp.pool_price, lp.lower_price, lp.upper_price);
-            if (!out_range) candidates.push(lp.pda);
-            return `${out_range ? '🚫' : '✅'}  *${lp.token_a} \\- ${lp.token_b}*  ${range_text}`
-        })
-        await ctx.reply(lines.join('\n\n'), { parse_mode: "MarkdownV2" });            
-
-        if (candidates.length) {
-            await ctx.reply("🔔  Notification On");
         }
     } catch (err) {
         await ctx.reply("🙅🏻‍♂️  Sorry, I can't recognise your wallet address.");
@@ -58,31 +58,31 @@ export async function address_handler(ctx, session) {
             delete session[ctx.message.from.id];
         }
     }
-} 
-
-export async function notify_handler(bot, session) {
-    const orca = await getContext();
-    for (const user in session) {
-        const processed = [];
-        const canditates = session[user]
-        try {
-            const lps = await getPositionsDetails(orca, canditates);
-            for (const lp of lps) {
-
-                if (lp.pool_price > lp.upper_price || lp.pool_price < lp.lower_price) {
-                    console.log('>>>> Timestamp', new Date().toLocaleString());
-                    console.log(">>>> out range");
-                    console.log(lp);
-                    const range_text = format(lp.pool_price, lp.lower_price, lp.upper_price);
-                    let msg = "🔔  Your LP is out of range:\n\n" +
-                        `🚫  *${lp.token_a} \\- ${lp.token_b}*  ${range_text}`;
-                    await bot.api.sendMessage(user, msg, { parse_mode: "MarkdownV2" });
-                    processed.push(lp.pda);
-                }
-            }
-        } catch (err) {
-            console.log(err.message);
-        }
-        session[user] = session[user].filter((address) => !processed.includes(address));
-    }
 }
+
+// export async function notify_handler(bot, session) {
+//     const orca = await getContext();
+//     for (const user in session) {
+//         const processed = [];
+//         const canditates = session[user]
+//         try {
+//             const lps = await getPositionsDetails(orca, canditates);
+//             for (const lp of lps) {
+
+//                 if (lp.pool_price > lp.upper_price || lp.pool_price < lp.lower_price) {
+//                     console.log('>>>> Timestamp', new Date().toLocaleString());
+//                     console.log(">>>> out range");
+//                     console.log(lp);
+//                     const range_text = format(lp.pool_price, lp.lower_price, lp.upper_price);
+//                     let msg = "🔔  Your LP is out of range:\n\n" +
+//                         `🚫  *${lp.token_a} \\- ${lp.token_b}*  ${range_text}`;
+//                     await bot.api.sendMessage(user, msg, { parse_mode: "MarkdownV2" });
+//                     processed.push(lp.pda);
+//                 }
+//             }
+//         } catch (err) {
+//             console.log(err.message);
+//         }
+//         session[user] = session[user].filter((address) => !processed.includes(address));
+//     }
+// }
